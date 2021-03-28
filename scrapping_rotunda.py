cce58c7d7cf34dd93c8cf688d642772c14e7cc2f
Leonardo Emanuel De Los Santos Agesta <leonardo.santos.agesta@fing.edu.uy>
2021-03-27 21:35:44 -0300

    :alien: scrapping for zara done

--- scrapping_rotunda.py
+++ scrapping_rotunda.py
@@ -1,14 +1,27 @@
 from requests import get
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
+import re
+import json
+import time
 
 def url_content(url, headers=None):
     response = get(url, headers= headers)
     return response.content
 
 if __name__ == '__main__':
-    url = 'https://www.zara.com/uy/es/sobrecamisa-cuadros-p06023519.html?v1=95823853&v2=1723554'
+    url = input("URL: ")
+    
     agent = UserAgent()
     header = {'User-Agent': agent.random}
-    soup = BeautifulSoup(url_content(url,header), 'html.parser')
-    print(soup.prettify())
+    website = url_content(url,header)
+    # website_content = BeautifulSoup(website, "html.parser")
+    pattern = re.compile(r'"sizes":.+?(?=])')
+    match = pattern.search(str(website))
+    if match:
+        string_lst = match.group().split('"sizes":', maxsplit=1)[-1] + ']'
+        result_json = json.loads(string_lst)
+        print(result_json)
+        for item in result_json:
+            print(f"Talle: {item['name']} --> {item['availability']}")
+    
